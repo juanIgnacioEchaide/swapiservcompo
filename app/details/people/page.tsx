73f9c677ab498d8a7a4f94fe0079fd0cb840eb47
juanichaide <jie_chaide@hotmail.com>
2023-10-01 00:06:01 -0300

    style fixes

--- app/details/people/page.tsx
+++ app/details/people/page.tsx
@@ -6,6 +6,10 @@ export default async function People() {
     const data = await api.getPeople()
 
     return (<div>
-        <ItemsListContainer items={data} title={'People'} pages={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} />
-    </div>)
+                <ItemsListContainer 
+                    items={data} 
+                    title={'People'} 
+                    pages={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} 
+                />
+            </div>)
 }
