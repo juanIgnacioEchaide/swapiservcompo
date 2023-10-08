55bf7e6fc8345c777edad8489d32507ce40968ce
juanichaide <jie_chaide@hotmail.com>
2023-10-08 03:34:04 -0300

    home animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
+import { HomeLink } from '@/components/atoms/page/HomeLink'
 import './global.css'
 import { DETAILS_ROUTE } from '@/utils/constants'
-import Link from 'next/link'
 
 export default async function Home() {
 
   return (
     <div className="home">
-      <Link href={DETAILS_ROUTE.PEOPLE}><div className="home__button">PEOPLE</div></Link>
-      <Link href={DETAILS_ROUTE.STARSHIP}><div className="home__button">STARSHIP</div></Link>
-      <Link href={DETAILS_ROUTE.PLANET}> <div className="home__button">PLANET</div></Link>
-      <Link href={DETAILS_ROUTE.FILMS}><div className="home__button">FILMS</div></Link>
-      <Link href={DETAILS_ROUTE.SPECIES}><div className="home__button">SPECIES</div></Link>
-      <Link href={DETAILS_ROUTE.VEHICLES}><div className="home__button">VEHICLES</div></Link>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+        }}>
+        <HomeLink route={DETAILS_ROUTE.PEOPLE} title={'PEOPLE'} imageUrl={''} />
+        <HomeLink route={DETAILS_ROUTE.STARSHIP} title={'STARSHIP'} imageUrl={''} />
+        <HomeLink route={DETAILS_ROUTE.PLANET} title={'PLANET'} imageUrl={''} />
+        <HomeLink route={DETAILS_ROUTE.FILMS} title={'FILMS'} imageUrl={''} />
+        <HomeLink route={DETAILS_ROUTE.SPECIES} title={'SPECIES'} imageUrl={''} />
+        <HomeLink route={DETAILS_ROUTE.VEHICLES} title={'VEHICLES'} imageUrl={''} />
+      </div>
     </div>
   )
 }
---
+++ components/atoms/page/HomeLink.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+interface HomeLinkProps {
+  title: string;
+  imageUrl: string;
+  route: string;
+}
+
+export function HomeLink({ title, imageUrl, route }: HomeLinkProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <Link href={route} passHref>
+      <div
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        style={{
+          textDecoration: 'none',
+          marginLeft: '1vh',
+          fontSize: '2vh',
+          color: 'white',
+          fontFamily: "'Mona-sans',system ui, sans-serif",
+          position: 'relative',
+          top: isHovered ? '-5%' : '0',
+          transition: 'top 0.3s ease',
+        }}
+      >
+        <div
+          style={{
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div
+            style={{
+              height: '40vh',
+              width: '15vw',
+              padding: '5vh',
+              textDecoration: 'none',
+              backgroundColor: 'green',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Image src={imageUrl} alt={title} width={300} height={300} />
+          </div>
+          <div
+            style={{
+              padding: '5vh',
+              textDecoration: 'none',
+              backgroundColor: 'black',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <h2 className="title">{title}</h2>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
